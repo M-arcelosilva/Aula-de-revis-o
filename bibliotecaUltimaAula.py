8100e3f87a3434a22c1d03691d6caf9ecf65869b
M-arcelosilva <128880538+M-arcelosilva@users.noreply.github.com>
2023-05-04 16:33:45 -0300

    Biblioteca e arquivo main

---
+++ bibliotecaUltimaAula.py
@@ -0,0 +1,18 @@
+
+def soma(valor1,valor2):
+    return valor1+valor2
+
+def multiplicacao(valor1,valor2):
+    return valor1*valor2
+
+def divisao(valor1,valor2):
+    return valor1/valor2
+
+def subtracao(valor1,valor2):
+    return valor1-valor2
+
+def divisaoNumeroInteiro(valor1,valor2):
+    return valor1//valor2
+
+def divisaoResto(valor1,valor2):
+    return valor1%valor2
---
+++ mainUltimaAula.py
@@ -0,0 +1,9 @@
+
+import bibliotecaUltimaAula as bbl 
+
+
+num1 = int(input('valor 1 '))
+num2 = int(input('valor 2 '))
+
+print(bbl.soma(num1,num2))
+print(bbl.multiplicacao(num1,num2))
